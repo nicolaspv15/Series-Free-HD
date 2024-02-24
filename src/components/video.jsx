1590f4ec42dc4e3923cf816c41b13bce82e3e94f
nicolaspv15 <nipovi2007@gmail.com>
2024-02-24 13:08:34 +0100

    Dime que es ya lo ultimo

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -12,29 +12,29 @@ const App = () => {
     <div className="column">
       <div className="div2">
         Chapter 1
-        <img className="imagen" width="35" src="public/Play.png" alt="Play" onClick={handleShowVideo} />
+        <img className="imagen" width="35" src="./public/play.png" alt="Play" onClick={handleShowVideo} />
       </div>
       <div className="div3">
         Chapter 2
-        <img className="imagen" width="35" src="public/Play.png" alt="Play" onClick={handleShowVideo} />
+        <img className="imagen" width="35" src="./public/play.png" alt="Play" onClick={handleShowVideo} />
       </div>
       <div className="div4">
         Chapter 3
-        <img className="imagen" width="35" src="public/Play.png" alt="Play" onClick={handleShowVideo} />
+        <img className="imagen" width="35" src="./public/play.png" alt="Play" onClick={handleShowVideo} />
       </div>
       <div className="div5">
         Chapter 4
-        <img className="imagen" width="35" src="public/Play.png" alt="Play" onClick={handleShowVideo} />
+        <img className="imagen" width="35" src="./public/play.png" alt="Play" onClick={handleShowVideo} />
       </div>
       <div className="div6">
         Chapter 5
-        <img className="imagen" width="35" src="public/Play.png" alt="Play" onClick={handleShowVideo} />
+        <img className="imagen" width="35" src="./public/play.png" alt="Play" onClick={handleShowVideo} />
       </div>
 
       {showVideo && (
         <div className="video-container">
           <video controls>
-            <source width="50" src="public/Video.mov" type="video/mp4" />
+            <source src="./public/video.mov" type="video/mp4" />
             Tu navegador no soporta el elemento de video.
           </video>
         </div>
